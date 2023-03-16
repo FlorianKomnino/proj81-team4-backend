a19714cf776b26a9ff9afbe7e6ef9ce96b380639
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-16 16:07:47 +0100

    create Apartment model

---
+++ app/Models/Apartment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Apartment extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'rooms',
+        'beds',
+        'bathrooms',
+        'square_meters',
+        'address',
+        'longitude',
+        'latitude',
+        'visible',
+        'image'
+    ];
+}

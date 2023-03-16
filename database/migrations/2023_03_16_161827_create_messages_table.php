4d1beb72f48e6140c6a2ebb32d007b5f3f7f779b
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-16 17:26:40 +0100

    create/add messages table create migration

---
+++ database/migrations/2023_03_16_161827_create_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('apartment_id')->after('id');
+            $table->foreign('apartment_id')
+                ->references('id')
+                ->on('apartments')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->text('text_content',3000);
+            $table->string('email',100);
+            $table->string('name',100)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};

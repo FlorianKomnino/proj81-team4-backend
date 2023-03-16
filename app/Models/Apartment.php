76c6121960c48db7203365a7c91b9803dd7b0427
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-16 18:25:26 +0100

    create/add Service model, implement apartment relation

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -27,4 +27,8 @@ class Apartment extends Model
     public function messages(){
         return $this->hasMany(Message::class);
     }
+
+    public function services(){
+        return $this->belongsToMany(Service::class);
+    }
 }

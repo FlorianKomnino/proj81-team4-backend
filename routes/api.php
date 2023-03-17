3419f7b45f669aba8bcf3f09d22695f6f48c40b3
FlorianKomnino <florian.komnino@outlook.it>
2023-03-17 13:00:24 +0100

    add Route::post /user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api_token')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/user', [UserController::class, 'store'])->name('api.user.store');

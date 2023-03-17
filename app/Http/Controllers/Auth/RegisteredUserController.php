974c84fb58aeca0f81171fadfefb321e979e35f8
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-17 10:22:11 +0100

    fix user fields surname, birth_date

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,6 +32,8 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'surname' => ['string', 'max:40'],
+            'birth_date' => ['date'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);

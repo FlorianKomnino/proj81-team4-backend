388b4e17e1ecde18a3d1497e65dd7dce86c139ec
FlorianKomnino <florian.komnino@outlook.it>
2023-03-17 18:39:57 +0100

    implement apiToken and  login function, edit password hashing

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,9 +6,44 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+    private $apiToken;
+    public function __construct()
+    {
+        $this->apiToken = uniqid(base64_encode(Str::random(40)));
+    }
+
+    /** 
+     * 
+     * @return \Illuminate\Http\Response 
+     */
+
+    public function login(Request $request)
+    {
+        //User check
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            //Setting login response 
+            $success['token'] = $this->apiToken;
+            $success['name'] =  $user->name;
+            return response()->json([
+                'status' => 'success',
+                'data' => $success
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'data' => 'Unauthorized Access'
+            ]);
+        }
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -62,6 +97,7 @@ class UserController extends Controller
             ]);
         }
 
+        $data['password'] = Hash::make($data['password']);
         $newUser = new User();
         $newUser->fill($data);
         $newUser->save();

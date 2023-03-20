6064747b0fb3745e2acfbc49b0fda8fdd42d2b49
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-20 11:30:17 +0100

    implement dynamic content in dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                     <div class="card-body">
                         <p class="text-center text-decoration-underline my-3">aggiungi/modifica foto</p>
                         <h4 class="text-success">Utente registrato</h4>
-                        <p class="text-secondary">email utente</p>
+                        <p class="text-secondary">{{Auth::user()->email}}</p>
                         <p>benvenuto nella dashboard del tuo profilo</p>
                         @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -25,8 +25,8 @@
                 </div>
 
                 <div class="col-8 px-5 py-4">
-                    <h2>Ciao nome utente!</h2>
-                    <p class="text-secondary">Su Bool BnB dal anno iscrizione</p>
+                    <h2>Ciao {{Auth::user()->name}}!</h2>
+                    <p class="text-secondary">Su Bool BnB dal {{substr(Auth::user()->created_at,0,4)}}</p>
                     <p class="text-decoration-underline my-3">modifica il profilo</p>
                     <a href="" class="btn btn-secondary">+ aggiungi appartamento</a><br>
                     <a href="" class="btn btn-secondary my-3">Messaggi</a><br>

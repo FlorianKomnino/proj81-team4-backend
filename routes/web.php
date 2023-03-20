3a0ea447f1a1e46dc3c68cd727012bd0040d2b62
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-20 12:26:58 +0100

    remove route - useless

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/createApartmentForm', function () {
-    return view('user.createApartmentForm');
-});
-
 Route::middleware(['auth', 'verified'])->prefix('user')->name('user.')->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');

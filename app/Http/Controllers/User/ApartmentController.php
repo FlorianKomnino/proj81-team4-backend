f9d23d7c93b02779424c28358ed059457e049496
FlorianKomnino <florian.komnino@outlook.it>
2023-03-20 11:31:34 +0100

    add $validationRules, create crud, store crud -- ApartmentController.php

--- app/Http/Controllers/User/ApartmentController.php
+++ app/Http/Controllers/User/ApartmentController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ApartmentController extends Controller
 {
+
+    protected $validationRules = [
+        'title' => 'required|unique:apartments|string|min:2|max:255',
+        'rooms' => 'number|min:1',
+        'beds' => 'number|min:1',
+        'bathrooms' => 'number|min:1',
+        'square_meters' => 'number',
+        'address' => 'string',
+        'image' => 'image|max:2048'
+    ];
+
+
     /**
      * Display a listing of the resource.
      *
@@ -19,12 +33,12 @@ class ApartmentController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
+     *@param Apartment $apartment
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Apartment $apartment)
     {
-        //
+        return view('user.apartment.create', ['apartment' => new Apartment()]);
     }
 
     /**
@@ -35,7 +49,24 @@ class ApartmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = $this->validationRules;
+        $data = $request->validate($rules);
+        $data['image'] =  Storage::put('imgs/', $data['image']);
+
+        $newApartment = new Apartment();
+        $newApartment->title = $data['title'];
+        $newApartment->rooms = $data['rooms'];
+        $newApartment->beds = $data['beds'];
+        $newApartment->bathrooms = $data['bathrooms'];
+        $newApartment->square_meters = $data['square_meters'];
+        $newApartment->address = $data['address'];
+        $newApartment->image = $data['image'];
+        // $newApartment->longitude = 'longitude' from tomtom;
+        // $newApartment->latitude = 'latitude' from tomtom;
+        // $newApartment->visible = TO DO;
+        $newApartment->save();
+
+        return redirect()->route('user.apartment.show', $newApartment->id)->with('message', "L\'appartamento '$newApartment->title', è stato creato con successo.");
     }
 
     /**

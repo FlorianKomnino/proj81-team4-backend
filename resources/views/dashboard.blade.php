aad6b97e487241161c0a0f7144561f25e86f30ee
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-20 12:06:10 +0100

    remove dashboard header from dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col">
             <div class="row">

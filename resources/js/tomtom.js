068e72f21e16747da1507bd3ab7a4d7a32c89b09
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-23 19:47:26 +0100

    implement function to make the input search box of TomTom and delete some useless functions

--- resources/js/tomtom.js
+++ resources/js/tomtom.js
@@ -1,101 +1,13 @@
 import tt from '@tomtom-international/web-sdk-maps'
 
+import { services } from '@tomtom-international/web-sdk-services';
+import SearchBox from '@tomtom-international/web-sdk-plugin-searchbox';
+
 let urlAddress = 'https://api.tomtom.com/search/2/search/';
 let positionUrlAddress = 'https://api.tomtom.com/search/2/geometryFilter.json';
-let apartmentsList = [
-                {
-                    position: {
-                        lat: 9.165437,
-                        lon: 45.464718,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.163100,
-                    lon: 45.478681,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.207381,
-                    lon: 45.483141,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.677238,
-                    lon: 45.698168,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.661306,
-                    lon: 45.682499,
-                    }
-                }
-            ];
 
-function getHouses() {
-    let locationQuery = document.getElementById('locationQuery').value;
-    console.log(locationQuery)
-    axios.get(urlAddress + `${locationQuery}.json`, {
-        params: {
-            key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
-            
-        }
-    })
-    .then((response) => {
-        console.log(response)
-        let centerCoordinate = response.data.results[0].position;
-        let position = `${response.data.results[0].position.lon},${response.data.results[0].position.lat}`;
-        axios.get(positionUrlAddress, {
-            params: {
-                key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
-                geometryList: JSON.stringify([
-                    {
-                    type: "CIRCLE",
-                    position: position,
-                    radius: 10000
-                    }
-            ]),
-            poiList: JSON.stringify(apartmentsList)
-        }
-        })
-        .then((response) => {
-            const map = tt.map({
-            key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
-            container: "map",
-            center: centerCoordinate,
-            zoom: 13
-            })
-            map.on('load', () => {
-                /* const MarkerEl = document.createElement("div");
-                const iconMarker = document.createElement("div");
-                iconMarker.innerHTML(iconMarker);
-                MarkerEl.classList.add('marker');
-                console.log(MarkerEl); */
-                response.data.results.forEach(function (location) {
-                    let marker = new tt.Marker().setLngLat([location.position.lat, location.position.lon]).addTo(map) 
-                    const popup = new tt.Popup({ anchor: 'top' }).setText('Posizione esatta fornita dopo la prenotazione.')
-                    marker.setPopup(popup)
-                })
-                
-            })
-            map.addControl(new tt.FullscreenControl());
-            map.addControl(new tt.NavigationControl());
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .finally(function () {
-
-        });
-    })
-    
-};
-
+// function to make the map appear in the show blade
 function map(locationQuery){
-
     let center = locationQuery
     const map = tt.map({
     key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
@@ -104,6 +16,8 @@ function map(locationQuery){
     zoom: 12
     })
     map.on('load', () => {
+            
+
             const iconMarker = document.getElementById('marker');
             console.log(iconMarker)
             const popup = new tt.Popup({ anchor: 'top' }).setText('Posizione esatta fornita dopo la prenotazione.')
@@ -113,17 +27,60 @@ function map(locationQuery){
     })
     map.addControl(new tt.FullscreenControl());
     map.addControl(new tt.NavigationControl());
+};
+
+// function to make the input search box of TomTom
+function getAddress(){
+    if (document.querySelector('.searchBar')){
+        const option = {
+            idleTimePress: 100,
+            minNumberOfCharacters: 0,
+            searchOptions: {
+                key: 'jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr',
+                language: 'it-IT',
+                limit: 10,
+            },
+            autocompleteOptions: {
+                key: 'jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr',
+                language: 'it-IT'
+            },
+            labels: {
+                noResultsMessage: 'Nessun risultato trovato'
+            },
+            distanceFromPoint: '9.192771,45.463273',
+            units: 'kilometers'
+        }
+        const ttSearchBox = new SearchBox(services, option);
+        const searchBarContainer = document.querySelector('.searchBar');
+        const searchBoxHTML = ttSearchBox.getSearchBoxHTML();
+        const inputAddress = document.createElement('input')
+        inputAddress.setAttribute('name', 'address')
+        inputAddress.classList.add('d-none')
+        searchBarContainer.append(inputAddress)
+        
+        searchBarContainer.append(searchBoxHTML);
+        function handleResultSelection(event) {
+            inputAddress.setAttribute('value', event.data.result.address.freeformAddress)
+            let results = event.data.result.address.freeformAddress;
+            let coordinate = results.position;
+        };
+        ttSearchBox.on("tomtom.searchbox.resultselected", handleResultSelection);
+    }
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
     let coordinate = document.getElementsByName('coordinate[]');
-    let k = [];
-    for (let i = 0; i < coordinate.length; i++) {
-        let a = coordinate[i]['defaultValue'];
-        k.push(a)
+    if (coordinate.length>0){
+        let k = [];
+        for (let i = 0; i < coordinate.length; i++) {
+            let a = coordinate[i]['defaultValue'];
+            k.push(a)
+        }
+        let lon = Number(k[0]);
+        let lat = Number(k[1]);
+        let coordinates = [lon, lat]
+        map(coordinates);
+    } else {
+        getAddress();
     }
-    let lon = Number(k[0]);
-    let lat = Number(k[1]);
-    let coordinates = [lon, lat]
-    map(coordinates);
-  });
+});

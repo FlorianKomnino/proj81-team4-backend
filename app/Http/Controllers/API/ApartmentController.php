5329547294bd09c01620477ce7f39b601b704caf
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-30 12:53:19 +0200

    implement function sponsoredApartments for sponsoredApartments API

--- app/Http/Controllers/API/ApartmentController.php
+++ app/Http/Controllers/API/ApartmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Apartment;
+use App\Models\Sponsorship;
 use App\Models\Message;
 use App\Models\Service;
 use Illuminate\Support\Facades\DB;
@@ -69,6 +70,23 @@ class ApartmentController extends Controller
         $receivedMessage->apartment_id = $data['apartment_id'];
         $receivedMessage->name = $data['name'];
         $receivedMessage->save();
+    }
+
+    public function sponsoredApartments(Sponsorship $sponsorship, Apartment $apartment)
+    {
+        $allSponsoredApartments = DB::table('apartment_sponsorship')->orderBy('id', 'desc')->get();
+        $idSponsoredApartmentsNow = [];
+        foreach ($allSponsoredApartments as $sponsoredApartment){
+            if ($sponsoredApartment->ending_time > now()){
+                array_push($idSponsoredApartmentsNow, $sponsoredApartment->apartment_id);
+            }
+            
+        }
+        $apartmentsToShow = Apartment::with('services')->whereIn('apartments.id', $idSponsoredApartmentsNow)->get();   
+        return response()->json([
+            'success' => true,
+            'results' => $apartmentsToShow
+        ]);
     }
 
     /**

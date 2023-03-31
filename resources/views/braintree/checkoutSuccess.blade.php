5788355c37162f0e9ee2116798789abd72def40f
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-31 16:27:26 +0200

    add checkoutSuccess style --prototype

--- resources/views/braintree/checkoutSuccess.blade.php
+++ resources/views/braintree/checkoutSuccess.blade.php
@@ -1,17 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div id="payment-checkout-success" class="container">
     <div class="row">
-        <div class="col-2">
+        <h2 class="col-12 my-3">
+            La <span class="brand-color">Transazione</span> è avvenuta con successo
+        </h2>
+        <p class="my-5 col-12 col-lg-10 offset-lg-1">Grazie per averci <span class="brand-color"></span> scelto! Ora il <span class="brand-color">tuo </span>appartamento ora risulterà <span class="brand-color">in evidenza</span> e potrà godere di <span class="brand-color">grande visibilità</span>! Non ti dimenticare di rendere <span class="brand-color">visibile</span> il tuo appartamento e ricorda di tenere d'occhio il tempo di sponsorizzazione restante per ogni tuo appartamento così da restare <span class="brand-color">primo</span> in classifica!</p>
+        <div class="col-12 col-md-6">
             <a href="{{ route('user.apartments.index' ) }}">Torna agli appartamenti</a>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-4">
-            <h1 class="text-success">
-                Transazione avvenuta con successo
-            </h1>
         </div>
     </div>
 </div>
